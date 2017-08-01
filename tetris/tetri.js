c22ebbdcdd59e4055fe1d63edf0f561187b5f1b4
Donapati Ravindra Kumar Reddy <dreddy@thoughtworks.com>
2017-08-01 19:52:47 +0530

    Create static obj
    -Roate Obj if required
    -Clears previous co-ordnates
    -Updates current co-ordinates

--- tetris/tetri.js
+++ tetris/tetri.js
@@ -1,5 +1,14 @@
-var movecolpos=3;
+var movecolpos=z=3;
 var prevcolpos=3;
+var rotate=false;
+var dd=[[1,0],[1,1]];
+var pre_cord=[  [ {x:1,y:1},  {x:1,y:1}   ],
+                [ {x:1,y:1},  {x:1,y:1}   ]
+             ];
+var current_cord = [  [ {x:0,y:0},  {x:0,y:0}   ],
+                [ {x:0,y:0},  {x:0,y:0}   ]
+             ];
+
 function init(){
     var rowno=10;
     var colno=5;
@@ -22,6 +31,7 @@ function init(){
     }
 
 }
+
 
 function slideObj(pos,dd){
   console.log(dd);
@@ -48,14 +58,50 @@ function slideObj(pos,dd){
      }
 
 }
+
+function clearPre(){
+  
+  for(var r=0;r<pre_cord.length;r++){
+    for(var c=0;c<pre_cord[r].length;c++){
+      var cor=pre_cord[r][c];
+       document.getElementById(10*cor['x']+cor['y']).classList.remove('game-cell','active-cell');
+       document.getElementById(10*cor['x']+cor['y']).classList.add('game-cell');
+    }
+  }
+
+}
+
+function updateCurrAndMove(k,z){
+     for(var r=0;r<current_cord.length;r++){
+    for(var c=0;c<current_cord[r].length;c++){
+          var cor=current_cord[r][c]={x:k+r,y:z+c};
+       document.getElementById(10*cor.x+cor.y).classList.remove('game-cell','active-cell');
+       if(dd[r][c]==1){
+         document.getElementById(10*(cor.x)+(cor.y)).classList.add('active-cell');
+        }else{
+         document.getElementById(10*(cor.x)+(cor.y)).classList.add('game-cell');
+        }
+    }
+  }
+}
+
 function tickElement(){
 var k=1;
 var current,previous;
   setInterval(function(){
-    var dd=[[1,0],[1,1]];
-
+  
     if(k<11){
-      slideObj(k,dd);
+        if(rotate){
+          var tmp_obj =[ [ dd[1][0] , dd[0][0] ] , [ dd[1][1] , dd[0][1] ] ];
+          dd = tmp_obj;
+          rotate = false;
+        }
+
+       
+        clearPre();
+        updateCurrAndMove(k,z);
+        pre_cord=current_cord;
+
         // current=document.getElementById(10*k+movecolpos);
         // previous=document.getElementById(10*(k-1)+prevcolpos);
         // if(current.classList.contains('active-cell')){
@@ -85,6 +131,9 @@ function right(){
  if(movecolpos<5){
    movecolpos++;
  }
+}
+function rot(){
+  rotate = true;
 }
 
 init();
